34c7f41d130b6aaa642318538ab56f4a1195288e
morocu <michael.orocu@utp.ac.pa>
2025-04-20 23:46:55 -0500

    creado de login,logout,y signup

--- blogproject/blogapp/urls.py
+++ blogproject/blogapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import BlogListView, BlogDetailView, ReviewCreateView, CommentCreateView, BlogCreateView
-
+from  . import views
 app_name = 'blogapp'
 
 
@@ -10,4 +10,8 @@ urlpatterns = [
     path('blog/<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
     path('blog/<int:pk>/review/', ReviewCreateView.as_view(), name='add_review'),
     path('blog/<int:blog_pk>/review/<int:review_pk>/comment/', CommentCreateView.as_view(), name='add_comment'),
+    path('registro/', views.registro, name="registro"),
+    path('login/', views.inicio_sesion, name='login'),
+    path('logout/', views.Cerrar_sesion, name='logout'),
+
 ]
--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -2,6 +2,57 @@ from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from .models import Blog, Review, Comment
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth import login, logout, authenticate
+from django.db import IntegrityError
+
+def registro(request):
+
+    if request.method == 'GET':
+        return render(request, 'registro.html',{
+            'form': UserCreationForm
+    })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            #registrarusuario  
+            try:
+                user = User.objects.create_user(username=request.POST['username'], 
+                password=request.POST['password1']) 
+                user.save()
+                login(request, user)
+                return redirect('blogapp:inicio_sesion')
+            except IntegrityError:
+                return render(request, 'registro.html',{
+                    'form': UserCreationForm,
+                    "Error": 'El Usuario ya existe'
+                })
+        return render(request, 'registro.html',{
+                    'form': UserCreationForm,
+                    "Error": 'Las contrase;as no coinciden'
+                })
+
+def inicio_sesion(request):
+    if request.method == 'GET':
+        return render(request, 'login.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'],password=request.POST['password'])
+        if user is None:
+            return render(request, 'login.html',{
+                'form': AuthenticationForm,
+                'Error': 'nombre de ususario o contrase;a incorrectos'
+                })
+        else:
+            login(request,user)
+            return redirect('blogapp:blog_list')
+
+def Cerrar_sesion(request):
+    logout(request)
+    return redirect('blogapp:blog_list')
 
 class BlogListView(ListView):
     model = Blog
@@ -53,3 +104,4 @@ class CommentCreateView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('blogapp:blog_detail', kwargs={'pk': self.kwargs['blog_pk']})
+
